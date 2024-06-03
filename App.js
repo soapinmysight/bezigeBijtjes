32b8746a263a3c348d1c7abfadb9f5cbd47bb77e
soapinmysight <dieuwvr@gmail.com>
2024-06-03 14:03:27 +0200

    setup project and file structure

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+        {/*mapview*/}
+      {/*<MapView style={styles.map}*/}
+      {/*  Region{{*/}
+
+      {/*}*/}
+      {/*         }*/}
+      {/*/>*/}
     </View>
+
   );
 }
 
