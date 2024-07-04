e2ededa62eb10f18a73174d3556a7af28d1e40f1
soapinmysight <dieuwvr@gmail.com>
2024-07-04 18:57:50 +0200

    added id's to json api. fixed api url.

--- src/services/loadLocationData.js
+++ src/services/loadLocationData.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
-
+// Hook for loading location data from API
 const useLoadingData = () => {
+    // State to hold location data
     const [locationData, setLocationData] = useState([]);
-    const API_URL = "https://raw.githubusercontent.com/soapinmysight/bezigeBijtjes/master/assets/api/locationData.json?token=GHSAT0AAAAAACRFT4BYAYDGO5DBRVL6KHJUZUGYZHA";
+    // Const to hold api url
+    const API_URL = "https://raw.githubusercontent.com/soapinmysight/bezigeBijtjes/master/assets/api/locationData.json?token=GHSAT0AAAAAACRFT4BZ6JODEH4HGLYXK4FYZUG2MBA"
 
     useEffect(() => {
         (async () => {
             try {
+                // Fetch data from the API
                 const response = await fetch(API_URL);
+                // Parse the response as JSON
                 const apiData = await response.json();
+                // Update the state with the fetched data
                 setLocationData(apiData);
             } catch (e) {
+                // Log an error message if the fetching fails
                 console.log('could not get location data');
             }
         })();
-    }, []);
+    }, []); // Empty dependency array to ensure the effect runs only once on mount
 
+    // Return the location data so it can be used
     return locationData;
 };
 
